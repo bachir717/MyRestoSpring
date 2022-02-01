ea68e812a9845e249558a13345554c989ca0f608
flav1-ann <Moitoi28>
2022-02-01 09:33:09 +0100

    first commit

---
+++ src/main/java/eu/ensup/MyResto/MyRestoApplication.java
@@ -0,0 +1,13 @@
+package eu.ensup.MyResto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/ensup/MyResto/MyRestoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensup.MyResto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
