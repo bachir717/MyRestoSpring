759abb66e8df695fcd13bdf62d8fd99c88f3db5c
Amélie NIOCHE <nioche.amelie@gmail.com>
2022-02-01 15:39:56 +0100

    Ajout de l'annotation '@ToString' et ajout  du test unitaire de UserService

--- src/main/java/eu/ensup/MyResto/domaine/User.java
+++ src/main/java/eu/ensup/MyResto/domaine/User.java
@@ -2,6 +2,7 @@ package eu.ensup.MyResto.domaine;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@ToString
 public class User implements UserDetails {
 
     @Id
