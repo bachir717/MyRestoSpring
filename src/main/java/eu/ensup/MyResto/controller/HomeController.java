e079cc6a049a969f67d0216193b52b950500f168
flav1-ann <Moitoi28>
2022-02-02 11:15:07 +0100

    front

--- src/main/java/eu/ensup/MyResto/controller/HomeController.java
+++ src/main/java/eu/ensup/MyResto/controller/HomeController.java
@@ -1,7 +1,9 @@
 package eu.ensup.MyResto.controller;
 
 
+import eu.ensup.MyResto.domaine.Product;
 import eu.ensup.MyResto.domaine.User;
+import eu.ensup.MyResto.model.Types;
 import eu.ensup.MyResto.service.AuthService;
 import eu.ensup.MyResto.service.OpinionsService;
 import eu.ensup.MyResto.service.ProductService;
@@ -12,6 +14,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static eu.ensup.MyResto.model.Types.*;
 
 @Log4j2
 @Controller
@@ -30,7 +37,36 @@ public class HomeController {
     @GetMapping("/")
     public String viewHome(Model model) {
         log.info("viewHome");
+        List<Product> entrees  = new ArrayList<Product>(){ };
+        List<Product> plats  = new ArrayList<Product>(){ };
+        List<Product> desserts  = new ArrayList<Product>(){ };
+        List<Product> boissons = new ArrayList<Product>(){ };
+
+        for (Product produc : productService.getAll()) {
+            switch (produc.getType()) {
+                case BOISSON:
+                    boissons.add(produc);
+                    break;
+                case ENTREE:
+                    entrees.add(produc);
+                    break;
+                case PLAT:
+                    plats.add(produc);
+                    break;
+                case DESSERT:
+                    desserts.add(produc);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         model.addAttribute("user", new User());
+        model.addAttribute("entrees",  entrees);
+        model.addAttribute("plats",  plats);
+        model.addAttribute("desserts",  desserts);
+        model.addAttribute("boissons",  boissons);
+
         return "home";
     }
 }
--- src/test/java/eu/ensup/MyResto/repository/ProductRepositoryTest.java
+++ src/test/java/eu/ensup/MyResto/repository/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package eu.ensup.MyResto.repository;
 
 import eu.ensup.MyResto.MyRestoApplication;
 import eu.ensup.MyResto.domaine.Product;
+import eu.ensup.MyResto.model.Types;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,7 @@ class ProductRepositoryTest {
 
     @Test
     public void TestSaveProduct() {
-        Product product = new Product(0L,"Crepe", 4.12f,"Plat",null);
+        Product product = new Product(0L,"Crepe", 4.12f, Types.PLAT,null);
         Product retrunSave =  productRepository.save(product);
         assertThat(product.getName(), equalTo(retrunSave.getName()));
     }
@@ -29,7 +30,7 @@ class ProductRepositoryTest {
     @Test
     public void TestGetAllProducts() {
         for (int i=0 ; i < 10 ; i++)
-            productRepository.save(new Product(Long.valueOf(i),"Crepe "+i, 4.12f,"Plat",null));
+            productRepository.save(new Product(Long.valueOf(i),"Crepe "+i, 4.12f,Types.PLAT,null));
 
         List<Product> allProducts = productRepository.findAll();
         assertThat(10, equalTo(allProducts.size()));
@@ -38,10 +39,10 @@ class ProductRepositoryTest {
     @Test
     public void TestGetOneUserByID() {
 
-        productRepository.save(new Product(3L,"Crepe 3", 4.12f,"Plat",null));
+        productRepository.save(new Product(3L,"Crepe 3", 4.12f,Types.PLAT,null));
 
         for (int i=0; i < 10 ; i++)
-            productRepository.save(new Product(Long.valueOf(i),"Crepe "+i, 4.12f,"Plat",null));
+            productRepository.save(new Product(Long.valueOf(i),"Crepe "+i, 4.12f,Types.PLAT,null));
 
         Product product = productRepository.getById(1L);
 
