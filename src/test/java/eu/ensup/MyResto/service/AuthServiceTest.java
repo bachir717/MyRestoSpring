30869f41d2a92c3619601fda8aa04afbc7445a3f
Amélie NIOCHE <nioche.amelie@gmail.com>
2022-02-02 17:20:18 +0100

    Mise a jour de l'existant et création des test qui n'existait pas

--- src/test/java/eu/ensup/MyResto/service/AuthServiceTest.java
+++ src/test/java/eu/ensup/MyResto/service/AuthServiceTest.java
@@ -3,9 +3,6 @@ package eu.ensup.MyResto.service;
 import eu.ensup.MyResto.domaine.User;
 import eu.ensup.MyResto.model.Roles;
 import eu.ensup.MyResto.repository.UserRepository;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
@@ -14,14 +11,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collection;
+import java.util.Optional;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,12 +27,6 @@ class AuthServiceTest
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Mock
-    private AuthenticationManager authenticationManager;
-
-//    @Mock
-//    private JwtProvider jwtProvider;
-
     @InjectMocks
     private AuthService authService;
 
@@ -48,20 +34,19 @@ class AuthServiceTest
     @DisplayName("Test sign up")
     public void testSignUp()
     {
-        User user = new User();
-        user.setUsername("admin");
+        User user = new User(0L,"name","LastName","email","address", Roles.USER,"mdp","",null,null);
 
-        User userResult = new User();
-        userResult.setUsername("admin");
-//        userResult.setPassword(authService.encodePassword("admin"));
-        userResult.setRole(Roles.USER);
+        User resu = new User();
+        resu.setUsername(user.getUsername());
+        resu.setPassword("admin");
+        resu.setRole(Roles.USER);
 
         // GIVEN
-        when(userRepository.save(user)).thenReturn(userResult);
+        when(userRepository.save(user)).thenReturn(resu);
 
         // WHEN
         final String result = authService.signup(user).toString();
-        MatcherAssert.assertThat("Test fail : ", user.toString(), Matchers.equalTo(result));
+        MatcherAssert.assertThat("Test fail : ", resu.toString(), Matchers.equalTo(result));
 
         // THEN
         verify(userRepository).save(user);
@@ -69,26 +54,18 @@ class AuthServiceTest
 
     @Test
     @DisplayName("Test sign in")
-    public void testSignIn() {
-        User user = new User();
-        user.setUsername("admin");
-
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-        Authentication authenticate = token;
-        SecurityContextHolder.getContext().setAuthentication(authenticate);
-
-        String resultExpect = Jwts.builder().setSubject(user.getUsername()).signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512)).compact();
+    public void testSignIn()
+    {
+        User user = new User(0L,"name","LastName","email","address", Roles.USER,"mdp","",null,null);
 
         // GIVEN
-//        when(authenticationManager.authenticate(token)).thenReturn(authenticate);
-//        when(jwtProvider.generateToken(authenticate)).thenReturn(resultExpect);
-//
-//        // WHEN
-//        final String result = authService.signin(user);
-////        MatcherAssert.assertThat("Test fail : ", resultExpect, Matchers.equalTo(result));
-//
-//        // THEN
-//        verify(authenticationManager).authenticate(token);
-//        verify(jwtProvider).generateToken(authenticate);
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+        // WHEN
+        final String result = authService.signin(user).toString();
+        MatcherAssert.assertThat("Test fail : ", user.toString(), Matchers.equalTo(result));
+
+        // THEN
+        verify(userRepository).findByUsername(user.getUsername());
     }
 }
--- src/test/java/eu/ensup/MyResto/service/UserServiceTest.java
+++ src/test/java/eu/ensup/MyResto/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package eu.ensup.MyResto.service;
 
 import eu.ensup.MyResto.domaine.User;
+import eu.ensup.MyResto.model.Roles;
 import eu.ensup.MyResto.repository.UserRepository;
 
 import org.assertj.core.api.Assertions;
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -25,24 +27,26 @@ class UserServiceTest
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     @InjectMocks
     private UserService userService;
 
     @Test
     @DisplayName("Test load an user who exist return an user")
     void TestLoadUserByUsername() {
-        User user = new User();
-        user.setUsername("admin");
+        User user = new User(0L,"name","LastName","email","address", Roles.USER,"mdp","",null,null);
 
         // GIVEN
-        when(userRepository.findByUsername("admin")).thenReturn(java.util.Optional.of(user));
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(java.util.Optional.of(user));
 
         // WHEN
-        final String result = userService.loadUserByUsername("admin").toString();
+        final String result = userService.loadUserByUsername(user.getUsername()).toString();
         MatcherAssert.assertThat("Test fail : ", result, Matchers.equalTo(user.toString()));
 
         // THEN
-        verify(userRepository).findByUsername("admin");
+        verify(userRepository).findByUsername(user.getUsername());
     }
 
     @Test
@@ -57,5 +61,33 @@ class UserServiceTest
 
         // THEN
         verify(userRepository).findByUsername("admin");
+    }
+
+    @Test
+    @DisplayName("Test save an user")
+    public void testSave() {
+        User user = new User(0L,"name","LastName","email","address", Roles.USER,"mdp","",null,null);
+
+        // GIVEN
+        when(userRepository.save(user)).thenReturn(user);
+        // WHEN
+        MatcherAssert.assertThat("Test fail : ", userService.save(user));
+        // THEN
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    @DisplayName("Test get one user by id")
+    public void testGetOne()
+    {
+        User user = new User(0L,"name","LastName","email","address", Roles.USER,"mdp","",null,null);
+
+        // GIVEN
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        // WHEN
+        final User result = userService.getOne(user.getId());
+        MatcherAssert.assertThat("Test fail : ", user.toString(), Matchers.equalTo(result.toString()));
+        // THEN
+        verify(userRepository).findById(user.getId());
     }
 }
