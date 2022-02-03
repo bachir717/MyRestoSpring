cb4c0389a8cfd999d94fbe3dc6c2b65299bc66b6
flav1-ann <Moitoi28>
2022-02-03 23:46:14 +0100

    front

--- src/main/resources/static/assets/script.js
+++ src/main/resources/static/assets/script.js
@@ -52,4 +52,10 @@ window.onscroll = function(){
     }
     
 }
-
+function add_product(idProduct) {
+    var request = new XMLHttpRequest();
+    var url = "my_resto/addProductShoppingCard/"+idProduct;
+    request.open("GET", url, true);
+    //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send();
+}
