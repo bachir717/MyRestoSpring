7a6a2ef00acb801f48f65b1f6a989e496e8d06ac
Cherif-Bachir <52203575+Cherif-Bachir@users.noreply.github.com>
2022-02-03 12:01:34 +0100

    Commit Charte etat product

--- src/main/java/eu/ensup/MyResto/controller/GraphController.java
+++ src/main/java/eu/ensup/MyResto/controller/GraphController.java
@@ -1,11 +1,21 @@
 package eu.ensup.MyResto.controller;
 
+import eu.ensup.MyResto.domaine.Orders;
+import eu.ensup.MyResto.domaine.Product;
+import eu.ensup.MyResto.model.States;
+import eu.ensup.MyResto.service.OrderService;
+import eu.ensup.MyResto.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class GraphController {
@@ -33,20 +43,41 @@ public class GraphController {
 
     }
 
-    /*
-    	@Autowired
-	private ProduitService service;
-	@PostMapping("/addProduit")
-	public String addProduit(@RequestBody Produit produit)
+    @Autowired
+	private ProductService productService ;
+    @Autowired
+    private OrderService  orderService ;
+	@GetMapping("/ListProduit")
+	public String addProduit(Model model)
 	{
-		return service.saveProduit(produit);
+        int nbProductDelivered = 0;
+        int nbProductCreated = 0;
+        int nbProductConceld = 0;
+        List<Orders> orders = (List<Orders>) orderService.getAll();
+        for(Orders order: orders){
+            if(order.getSate() == States.DELIVERED)
+                nbProductDelivered++;
+            if(order.getSate() == States.CREATED)
+                nbProductCreated++;
+            if(order.getSate() == States.CANCELED)
+                nbProductConceld++;
+        }
+        //model.addAttribute("nbProductDelivered ",nbProductDelivered);
+        Map<String, Integer> data = new LinkedHashMap<String, Integer>();
+        data.put("nbProductDelivered", nbProductDelivered);
+        data.put("nbProductCreated", nbProductCreated);
+        data.put("nbProductConceld", nbProductConceld);
+        //data.put("Totale des produit", orders.size());
+        model.addAttribute("keySet", data.keySet());
+        model.addAttribute("values", data.values());
+        return "barChart";
 	}
-	@GetMapping("/barChart")
-	public String getAllProduit(Model model) {
-	List<String> nameList= service.getAllProduit().stream().map(x->x.getName()).collect(Collectors.toList());
-	List<Integer> priceList = service.getAllProduit().stream().map(x-> x.getPrice()).collect(Collectors.toList());
-	model.addAttribute("name", nameList);
-	model.addAttribute("price", priceList);
-	return "barChart";
-	}*/
+//	 @GetMapping("/barChart")
+//	public String getAllProduit(Model model) {
+//	List<String> nameList= service.getAllProduit().stream().map(x->x.getName()).collect(Collectors.toList());
+//	List<Integer> priceList = service.getAllProduit().stream().map(x-> x.getPrice()).collect(Collectors.toList());
+//	model.addAttribute("name", nameList);
+//	model.addAttribute("price", priceList);
+//	return "barChart";
+//	}
 }
